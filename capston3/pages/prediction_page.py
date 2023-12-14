29053464aa0ac9df08a2996221c74171d12d4af9
RahafAlshahrany <143160674+RahafAlshahrany@users.noreply.github.com>
2023-12-14 13:01:38 +0300

    Update prediction_page.py

--- capston3/pages/prediction_page.py
+++ capston3/pages/prediction_page.py
@@ -5,7 +5,6 @@ import pickle
 from streamlit_lottie import st_lottie
 
 # Load the model
-#filename = r"C:\Users\96655\Desktop\capston3\pages\rf_model_f.pkl"C:\Users\96655\Desktop\capston3\pages\trained_model
 filename = r"C:\Users\96655\Desktop\capston3\pages\trained_model"
 model = pickle.load(open(filename, "rb"))
 
@@ -26,27 +25,19 @@ def predict_fraud(data, entry=None):
         X = data
 
     if entry:
-        # Convert entry dictionary to numeric array-like format
         entry_array = np.array(list(entry.values())).reshape(1, -1)
-
-        # Predict for a specific entry
         prediction = model.predict(entry_array)
         return prediction
     else:
-        # Predict for all entries
         predictions = model.predict(X)
         return predictions
 
 # Main function
 def main():
-    # Center-aligned title with increased height and black color
-    st.markdown("<h1 style='text-align: center; color: white;'> Credit Card Prediction</h1>", unsafe_allow_html=True)
-
-    # Display Lottie animation with increased height
+    st.markdown("<h1 style='text-align: center; color: white;'>credit card fraud prediction</h1>", unsafe_allow_html=True)
     animation_url = "https://lottie.host/437a402f-a348-4cb4-bc75-b60fb0c77725/RkipOfCtFJ.json"
     st_lottie(animation_url, speed=1, height=800, key="animation")
 
-    # Sidebar options
     st.sidebar.title("Options")
     option = st.sidebar.radio("Select an option", ("Upload CSV", "Enter Values"))
 
@@ -62,30 +53,22 @@ def main():
                 predictions = predict_fraud(data)
                 st.subheader("Predictions")
 
-                # Center-align the entire "Predictions" section
                 st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
-
-                # Add styling to the table (set border properties)
                 st.write("<style>table {margin: 0 auto; border: 1px solid black; border-collapse: collapse;}</style>", unsafe_allow_html=True)
                 
-                # Map predictions to "Safe Transaction" or "Likely Fraud"
                 prediction_texts = ["Safe Transaction" if pred == 0 else "Likely Fraud" for pred in predictions]
 
-                # If there's only one prediction, convert it to DataFrame
                 if len(predictions) == 1:
                     st.table(pd.DataFrame({"Prediction": [prediction_texts[0]]}).style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}]))
                 else:
-                    # Center-align the table
                     prediction_df = pd.DataFrame({"Prediction": prediction_texts})
                     st.table(prediction_df.style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}]))
 
-                # Close the center-align div
                 st.markdown("</div>", unsafe_allow_html=True)
 
     elif option == "Enter Values":
         st.sidebar.subheader("Enter Credit Card Details")
 
-        # Ranges for specific entries
         ranges = {
             'V12': (-18.6837, 7.8484),
             'V17': (-25.1628, 9.253526),
@@ -99,24 +82,20 @@ def main():
             'V7': (-43.5572, 120.5895)
         }
 
-        # Collect credit card details from the user
         entry = {}
 
         for column, (min_val, max_val) in ranges.items():
             value = st.sidebar.text_input(column, type="default", placeholder=f"Enter value ({min_val} to {max_val})")
             
-            # Validation for non-numeric values
-            if not value.replace('.', '', 1).isdigit() and value:
+            if not value.replace('.', '', 1).lstrip('-').isdigit() and value:
                 st.sidebar.warning(f"Invalid entry for {column}. Please enter a numeric value.")
             else:
-                # Validation for value within the specified range
                 if value and not (min_val <= float(value) <= max_val):
                     st.sidebar.warning(f"Please enter a value for {column} within the range {min_val} to {max_val}.")
                 else:
                     entry[column] = float(value) if value else None
 
         if st.sidebar.button("Predict"):
-            # Validation for empty values
             missing_values = [col for col, val in entry.items() if val is None]
             
             if missing_values:
@@ -124,28 +103,21 @@ def main():
             else:
                 data = pd.DataFrame([entry])
 
-                # Check if the entry dictionary is empty before making predictions
                 if not entry:
                     st.sidebar.warning("Please enter values before predicting.")
                 else:
                     predictions = predict_fraud(data, entry)
                     st.subheader("Prediction")
 
-                    # Center-align the entire "Predictions" section
                     st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
-                    # Add styling to the table (set border properties)
                     st.write("<style>table {margin: 0 auto; border: 1px solid black; border-collapse: collapse;}</style>", unsafe_allow_html=True)
-                    # Map predictions to "Safe Transaction" or "Likely Fraud"
                     prediction_texts = ["Safe Transaction" if pred == 0 else "Likely Fraud" for pred in predictions]
-                    # If there's only one prediction, convert it to DataFrame
                     if len(predictions) == 1:
                         st.table(pd.DataFrame({"Prediction": [prediction_texts[0]]}).style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}]))
                     else:
-                        # Center-align the table
                         prediction_df = pd.DataFrame({"Prediction": prediction_texts})
                         st.table(prediction_df.style.set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}]))
 
-                    # Close the center-align div
                     st.markdown("</div>", unsafe_allow_html=True)
 
 # Run the app
